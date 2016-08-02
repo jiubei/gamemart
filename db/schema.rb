677f21d79466b09c876941b88c2c707d46eca3d3
Nicholas Lai <nicholas.lai1985@gmail.com>
2016-08-02 15:18:12 +0800

    Make a basic User model with password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160801022631) do
+ActiveRecord::Schema.define(version: 20160802051555) do
 
   create_table "reviews", force: :cascade do |t|
     t.text     "content"
@@ -24,8 +24,10 @@ ActiveRecord::Schema.define(version: 20160801022631) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
