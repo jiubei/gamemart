f0c5c32167dbe0bb30fe413ba8d2751b3c953a35
Nicholas Lai <nicholas.lai1985@gmail.com>
2016-08-03 14:35:43 +0800

    Impement basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,11 +10,13 @@ class SessionsController < ApplicationController
       # Log the user in and redirect to the user's show page.
     else
       # Create an error message.
-      flash.now[:danger] = "Invalid username/password combination" #Not quite right!
+      flash.now[:danger] = "Invalid username/password combination"
       render 'new'
     end
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
