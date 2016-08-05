0173f1d7151d2a9b386058f72782f59609c044dd
Nicholas Lai <nicholas.lai1985@gmail.com>
2016-08-05 11:03:48 +0800

    modified order of migrations so that tables to be ref are created first

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160804070059) do
+ActiveRecord::Schema.define(version: 20160803134545) do
 
   create_table "games", force: :cascade do |t|
     t.string   "name"
